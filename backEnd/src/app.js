378bb8001598c70bcbe013bc35c115ceeb193402
fehbarea <fehbarea@icloud.com>
2025-10-28 22:52:17 -0300

    feat: adicao validacao login

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,13 +1,21 @@
 import dotenv from 'dotenv';
 import express from 'express';
+//ROTAS
+import loginRoutes from './routes/loginRoute.js';
 import voluntarioRoutes from './routes/voluntarioRoute.js'
-
 
 dotenv.config();
 const app = express();
 
 app.use(express.json())
 
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+//ROTAS
+app.use('/login', loginRoutes);
 app.use('/voluntarios', voluntarioRoutes)
 
 export default app;
