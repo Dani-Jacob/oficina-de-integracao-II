fd8ed1604212515fa90a8763e5b860997193fe5c
fehbarea <fehbarea@icloud.com>
2025-10-29 22:37:44 -0300

    feat: adicao do crud certificados

--- backEnd/src/routes/voluntarioRoute.js
+++ backEnd/src/routes/voluntarioRoute.js
@@ -1,10 +1,22 @@
 import express from 'express'
-import { getVoluntario } from '../controller/voluntarioController.js'
-import validarToken from "../middlewares/authMiddleware.js"
+import { getVoluntario } from '../controllers/voluntarioController.js'
+import validarToken from '../middlewares/authMiddleware.js'
+import { listarCertificados, adicionarCertificado, atualizarCertificado, removerCertificado } from '../controllers/certificadoController.js'
 
 const router = express.Router()
 
-router.get('/:id', validarToken, getVoluntario)
+router.use(validarToken);
+
+router.get('/:id', getVoluntario)
+
+router.get('/:id/certificados', listarCertificados);
+
+router.post('/:id/certificados', adicionarCertificado);
+
+router.put('/:id/certificados/:certificadoId', atualizarCertificado);
+
+router.delete('/:id/certificados/:certificadoId', removerCertificado);
+
 
 export default router
 
