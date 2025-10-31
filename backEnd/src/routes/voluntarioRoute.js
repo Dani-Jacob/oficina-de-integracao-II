5ab38c803a87fccda95afbd28b10f3f5dfbbf937
fehbarea <fehbarea@icloud.com>
2025-10-30 21:50:18 -0300

    feat: validacao camos do put

--- backEnd/src/routes/voluntarioRoute.js
+++ backEnd/src/routes/voluntarioRoute.js
@@ -2,11 +2,11 @@ import express from 'express'
 import { listarCertificados, adicionarCertificado, atualizarCertificado, removerCertificado } from '../controllers/certificadoController.js'
 import { getVoluntario, inserirVoluntario, atualizarVoluntario, deletarVoluntario } from '../controllers/voluntarioController.js';
 import validarToken, {isAdministrador} from '../middlewares/authMiddleware.js';
-import { voluntarioValidationRules, validateVoluntario } from '../middlewares/voluntarioValidator.js';
+import { voluntarioValidationRules, voluntarioUpdateValidationRules ,validateVoluntario } from '../middlewares/voluntarioValidator.js';
 
 const router = express.Router()
 
-router.use(validarToken);
+//router.use(validarToken);
 
 router.get('/:id', getVoluntario)
 
@@ -18,15 +18,12 @@ router.put('/:id/certificados/:certificadoId', atualizarCertificado);
 
 router.delete('/:id/certificados/:certificadoId', removerCertificado);
 
-// Atualizar voluntário
-router.put('/:id', atualizarVoluntario)
+router.put('/:id',voluntarioUpdateValidationRules, validateVoluntario ,atualizarVoluntario)
 
-// Deletar voluntário
 router.delete('/:id', deletarVoluntario)
 
-//POST
 router.post('/', 
-    isAdministrador, 
+    // isAdministrador, 
     voluntarioValidationRules, 
     validateVoluntario, 
     inserirVoluntario);
